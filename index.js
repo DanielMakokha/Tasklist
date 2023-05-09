6ffc67d34df931f81a7affd083a0480d185ee9d3
DanielMakokha <danielmakokhan@gmail.com>
2023-05-09 15:02:46 +0300

    prevent default load

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import Task from './task.js';
 const ui = new UI();
 document.querySelector('.AddTaskBtn').addEventListener('click', e => {
     const taskTitle=document.querySelector('#newtaskID').value;
+    if (taskTitle.length>0) {
+        const task = new Task(taskTitle);
+        ui.addToUI(task);
+        ui.resetForm();
+        console.log(task);  
+    }
     
-    const task = new Task(taskTitle);
-    ui.addToUI(task);
-    ui.resetForm();
-    console.log(task);
 });
