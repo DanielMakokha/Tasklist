77eb159a7181a9ca53bb8240c7b5c5b848deb9ee
DanielMakokha <danielmakokhan@gmail.com>
2023-05-09 14:52:50 +0300

    modified the gui part

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+import UI from './ui.js';
+import Task from './task.js';
+
+const ui = new UI();
 document.querySelector('.AddTaskBtn').addEventListener('click', e => {
     const taskTitle=document.querySelector('#newtaskID').value;
-    console.log(taskTitle);
+    
+    const task = new Task(taskTitle);
+    ui.addToUI(task);
+    ui.resetForm();
+    console.log(task);
 });
