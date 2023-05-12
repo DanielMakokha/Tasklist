5791665eb471a0a4026f32d951dbb9d2aa1be398
DanielMakokha <danielmakokhan@gmail.com>
2023-05-12 13:38:18 +0300

    updated the delete part

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ document.querySelector('.AddTaskBtn').addEventListener('click', e => {
     }
     
 });
+
+document.querySelector('.task-list').addEventListener('click',(e)=>{
+    console.log(e.target.className);
+    if (e.target.className.includes('task__op_delete')) {
+        ui.deleteTask(e);
+    }
+})
